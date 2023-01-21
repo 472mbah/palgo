67abae4f6715b3208d6bd595f4b95ed980433584
Momodou Bah <472mbah@gmail.com>
2023-01-21 10:11:54 +0000

    CHecking value is permutation of a palindrome with hashmap to store counts

---
+++ strings/1-4.py
@@ -0,0 +1,36 @@
+"""
+Permutation of strings problem
+Approach: Take count of all values & ensure that
+- at most one character with odd count
+- all others must be even
+
+Casing can be ignored
+Non-letter characters can be ignored too 
+"""
+
+def isPermPalindrome (word):
+
+	characters = {}
+	word =  word.lower()
+	ranges = [ ord('a'), ord('z') ]
+	for x in word:
+		ord_ = ord(x)
+	
+		# If character is in range a - z
+		if ord_ < ranges[0] and ord_ > ranges[1]:
+			continue
+		
+		if x in characters:
+			characters[x] += 1
+		else:
+			characters[x] = 1
+	
+	oddCount = 0
+
+	for key in characters:
+		# if character is even
+		if characters[key] % 2 != 0:
+			oddCount+=1
+	
+	return oddCount <= 1		
+
