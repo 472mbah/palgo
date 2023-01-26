be792ff894ca0df25fba206c96676087cec8de01
Momodou Bah <472mbah@gmail.com>
2023-01-26 19:08:57 +0000

    Zero matrix solution 1

--- strings/1-8.py
+++ strings/1-8.py
@@ -1,8 +1,42 @@
+def setRowColsToZero (arr, i, j, M, N):
+	
+	# set row to zero
+	pointer = 0
+	
+	while pointer < N:
+		arr[i][pointer] = 0
+		pointer+=1
+	
+	# set col to 0
+	pointer = 0
+	while pointer < M:
+		arr[pointer][j] = 0
+		#print(arr[pointer][j])
+		pointer+=1
+	
+
+
 def zeroMatrix (arr):
-	i = 0
-	j = 0
-	store = {}
+
+	M = len(arr)
+	if M <= 0:
+		return
+
+	N = len(arr[0])
+	columnsCompleted = {}
+	rowsCompleted = {}
+	
 	# a strore will enable us to prevent re-doing operations
 
-	while j < len(arr):
-		
+	for i in range(M):
+		for j in range(N):
+			if i in rowsCompleted or j in columnsCompleted:
+				continue
+			if arr[i][j] == 0:
+				setRowColsToZero(arr, i, j, M, N)
+				columnsCompleted[j] = None
+				rowsCompleted[i] = None
+	return arr
+
+vector = zeroMatrix([ [1, 2, 3], [4, 0, 6], [7, 8, 9], [11, 12, 13]  ])
+[print(row) for row in vector]	
