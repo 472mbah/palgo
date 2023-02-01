dd2282c3a12030fe899b07fe992f036674f35703
Momodou Bah <472mbah@gmail.com>
2023-02-01 22:21:35 +0000

    improved appoach for 2-4

--- linkedlist/2-4.py
+++ linkedlist/2-4.py
@@ -1,19 +1,29 @@
 from linked import createRandomSet
 
-def partition (sequence, x, beforeX=None, afterX=None):
-	ignoreNode = False
-	if afterX == None:
-		if sequence.data == x:
-			afterX = sequence.next
-			ignoreNode = True
-	
-	beforeX = sequence if beforeX == None else beforeX
-	partition(sequence, x, beforeX, afterX)
-	
-	if ignoreNode:
+def partition (sequence, x, parentOfX=None, itemX=None, biggerThanX=None):
+	"""
+	Are we in mode 1
+	We can still look for items greater than x but positioned before x
+	parentsOfX and itemX get defined together
+	"""
+	if sequence.next == None:
 		return
 	
+	if parentOfX == None: 
+		
+		if biggerThanX == None:
+			sequence.data > x:
+				biggerThanX = sequence
+		
+		if sequence.next.data == x:
+			parentOfX = sequence
+			itemX = parentOfX.next
+			if biggerThanX != None: # this would could be an edge case where biggerThanX == sequence(current)
+				
+							
 
-		
-		
+	
+
+	# mode 2 - we just keep iterating and appending to the parent of x 
+	# the values which are smaller than x
 	
