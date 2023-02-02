a49b26d95a37857c4abac70a203b6df40c584e5e
Momodou Bah <472mbah@gmail.com>
2023-02-02 08:56:38 +0000

    upgrade to 2-4

--- linkedlist/2-4.py
+++ linkedlist/2-4.py
@@ -18,12 +18,23 @@ def partition (sequence, x, parentOfX=None, itemX=None, biggerThanX=None):
 		if sequence.next.data == x:
 			parentOfX = sequence
 			itemX = parentOfX.next
-			if biggerThanX != None: # this would could be an edge case where biggerThanX == sequence(current)
+			if biggerThanX != None: 
+				# this would could be an edge case where biggerThanX == sequence(current)
+				if biggerThanX == sequence:
+					# swap 6 and 5
+					# 6 becomes the child of 5
+					tailOfX = sequence.next.next
+					sequence.next = tailOfX
+					sequence.next.next = sequence
+					# 5 becomes the parent of 6
+					biggerThanX = sequence.next.next	
 				
-							
-
-	
-
+					
+		partion(biggerThanX, x, parentOfX, itemX, None) 				
+		
 	# mode 2 - we just keep iterating and appending to the parent of x 
 	# the values which are smaller than x
-	
+	else:
+		
+		if sequence.data < x:
+			pass
