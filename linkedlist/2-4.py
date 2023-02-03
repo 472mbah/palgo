9fa839f6027085aa83d0809c246335be1759ce35
Momodou Bah <472mbah@gmail.com>
2023-02-03 08:57:04 +0000

    Improved idea to 2-4

--- linkedlist/2-4.py
+++ linkedlist/2-4.py
@@ -1,40 +1,42 @@
-from linked import createRandomSet
+from linked import produceSequenceFromArray
 
-def partition (sequence, x, parentOfX=None, itemX=None, biggerThanX=None):
-	"""
-	Are we in mode 1
-	We can still look for items greater than x but positioned before x
-	parentsOfX and itemX get defined together
-	"""
+def partition ( sequence, x, pivotParent=None, previousNode=None, startNode=None):
+	
+	if sequence == None:
+		return
+	
 	if sequence.next == None:
 		return
 	
-	if parentOfX == None: 
-		
-		if biggerThanX == None:
-			sequence.data > x:
-				biggerThanX = sequence
-		
+	if startNode == None:
+		startNode = sequence
+
+	if pivotParent==None:
+	
 		if sequence.next.data == x:
-			parentOfX = sequence
-			itemX = parentOfX.next
-			if biggerThanX != None: 
-				# this would could be an edge case where biggerThanX == sequence(current)
-				if biggerThanX == sequence:
-					# swap 6 and 5
-					# 6 becomes the child of 5
-					tailOfX = sequence.next.next
-					sequence.next = tailOfX
-					sequence.next.next = sequence
-					# 5 becomes the parent of 6
-					biggerThanX = sequence.next.next	
-				
-					
-		partion(biggerThanX, x, parentOfX, itemX, None) 				
 		
-	# mode 2 - we just keep iterating and appending to the parent of x 
-	# the values which are smaller than x
+			if x < sequence.data:
+				previousNode.next = sequence.next
+				temp = sequence.next.next
+				sequence.next.next = sequence
+				sequence.next = temp
+				pivotParent = previousNode	
+			else:
+				pivotParent = sequence
+	
+		partition(sequence.next, x, pivotParent, sequence, startNode)
+	
 	else:
-		
+		expectedNext = sequence.next
 		if sequence.data < x:
-			pass
+			pivotParent.next = sequence
+			pivotParent.next.next = expectedNext	
+		
+		partition(expectedNext, x, pivotParent, pivotParent.next, startNode)
+	
+	pass
+
+sequence = produceSequenceFromArray([1, 2, 6, 5, 7, 4, 2, 1]) 
+sequence.printNodes()
+partition(sequence, 5)
+sequence.printNodes()
