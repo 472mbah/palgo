54584af588d57d220a977bcf2ca9fd773bdce34d
Momodou Bah <472mbah@gmail.com>
2023-02-07 20:17:15 +0000

    Attempting 2-6

---
+++ linkedlist/2-6.py
@@ -0,0 +1,12 @@
+def isPalindrome (sequence):
+	p1 = sequence
+	p2 = None
+	
+	
+
+	# get p2 to the end, probably wouldn't work though since we are not working with a doubly linked list
+	while temp.next != None:
+		p2 = temp
+		temp = temp.next
+	
+	 
