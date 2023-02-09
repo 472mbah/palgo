01d7f9099247fb1111e4b1f9a8b16e8076e60d4a
Momodou Bah <472mbah@gmail.com>
2023-02-09 20:25:43 +0000

    Is palindrome question for linked list

--- linkedlist/2-6.py
+++ linkedlist/2-6.py
@@ -1,12 +1,28 @@
+import math
+from linked import produceSequenceFromArray
 def isPalindrome (sequence):
 	p1 = sequence
-	p2 = None
+	p2 = sequence
+	temp = sequence
+	size = 0
 	
-	
-
-	# get p2 to the end, probably wouldn't work though since we are not working with a doubly linked list
+	# Identify the size
 	while temp.next != None:
-		p2 = temp
+		size+=1
 		temp = temp.next
+
+	midpoint = math.floor(size/2) 
+	while midpoint >= 0:
+		p2 = p2.next
+		midpoint-=1
+
+	while p1 != None and p2!=None:
+		if p1.data != p2.data:
+			return False
+		p1 = p1.next
+		p2 = p2.next
 	
-	 
+	return True
+	
+sequence = produceSequenceFromArray(list("aaabaaa"))
+print(isPalindrome(sequence)) 
